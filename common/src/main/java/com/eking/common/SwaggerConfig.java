3ac0d322ba43d88d61ec446f3cf14b733ad53562
00165650 <caozk929@163.com>
2025-03-10 17:52:42 +0800

    增加swagger文档配置

---
+++ common/src/main/java/com/eking/common/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package com.eking.common;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author HHHY
+ * @ClassName
+ * @Date: 2024/4/2 14:21
+ * @Description: Swagger 配置
+ */
+@Configuration
+public class SwaggerConfig {
+
+    @Value("${springdoc.info.title:}")
+    private String title;
+    @Value("${springdoc.info.desc:}")
+    private String desc;
+    @Value("${springdoc.info.version:}")
+    private String version;
+
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI().info(new Info().title(title)
+                .description(desc)
+                .version(version));
+    }
+}
