d6b83f8774c1733aaff9b7dd880a0cec6201b547
00165650 <caozk929@163.com>
2025-03-18 10:43:40 +0800

    web测试接口示例

--- web/src/main/java/com/eking/web/controller/TestWebController.java
+++ web/src/main/java/com/eking/web/controller/TestWebController.java
@@ -1,5 +1,7 @@
 package com.eking.web.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.Data;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,13 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * @author caozk929
  */
+@Tag(name = "Web开放测试接口", description = "Web开放测试接口描述")
 @Data
 @RestController
 @RequestMapping("/web")
-public class WebController {
+public class TestWebController {
 
+    @Operation(summary = "hello测试接口", description = "hello测试接口描述")
     @GetMapping("/hello")
     public String hello() {
         return "Hello from Web Controller for APP!";
     }
+
 }
