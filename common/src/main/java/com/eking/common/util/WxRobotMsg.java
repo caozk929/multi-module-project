5dacc3414cfbca058f201ee2ecbbfd458b2919fa
00165650 <caozk929@163.com>
2025-10-14 18:11:06 +0800

    chore(deps): 升级FastJSON到FastJSON2版本
    - 将common模块中的FastJSON依赖从com.alibaba:fastjson替换为com.alibaba.fastjson2:fastjson2- 更新根pom.xml中定义的FastJSON版本属性，从fastjson.version改为fastjson2.version，并升级至2.0.33版本- 在common模块的WxRobotMsg类中，将导入的FastJSON包路径从com.alibaba.fastjson更新为com.alibaba.fastjson2
    - 统一项目中FastJSON库的版本管理，确保依赖的一致性和安全性

--- common/src/main/java/com/eking/common/util/WxRobotMsg.java
+++ common/src/main/java/com/eking/common/util/WxRobotMsg.java
@@ -1,8 +1,9 @@
 
 package com.eking.common.util;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONArray;
+
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONArray;
 import lombok.Data;
 import org.apache.commons.lang3.StringUtils;
 
